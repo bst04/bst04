57f68ecdcc0ad094cad973b54e2192f6d0250707
cybst04 <75524846+bst04@users.noreply.github.com>
2025-11-28 13:44:57 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const elements = document.querySelectorAll(".fade-in");
-    elements.forEach((el, index) => {
-        el.style.animationDelay = `${index * 0.2}s`;
-    });
-});
